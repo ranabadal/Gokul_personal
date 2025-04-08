7d8dd55a09d5e4051c516846ad60de08c59b037c
ranabadal <ranabadal3004@gmail.com>
2025-04-08 13:30:45 +0530

    about page updated

--- client/src/pages/about/about.jsx
+++ client/src/pages/about/about.jsx
@@ -1,3 +1,41 @@
+// import React from "react";
+// import styles from "./about.module.css";
+// import AboveHeader from "../../components/above_header/above_header";
+// import Header from "../../components/header/header";
+// import Footer from "../../components/footer/footer";
+// import Title from "./Title/title";
+// import Cards from "./cards/cards";
+// import Banner from "./banner/banner";
+// import Herosection from "./hero_section/herosection";
+// import Testimonial from "./testimonial/testimonial";
+// import Facilities from "../../components/facilities/facilities"
+ 
+// function About () {
+//     return(
+//         <div>
+//         <div><AboveHeader /></div>
+//         <div><Header /></div>
+//         <div><Herosection /></div>
+//         <div><Banner /></div>
+//         <div><Herosection /></div>
+//         <div><Banner /></div>
+//         <div className={styles.title}>
+//            <Title />
+//            </div>
+//         <div className={styles.center}>
+//             {/* <Testimonial /> */}
+          
+//             <Cards />
+//             <Facilities />
+//         </div>
+//         <div><Footer /></div>
+//         </div>
+//     )
+// }
+// export default About;
+
+
+
 import React from "react";
 import styles from "./about.module.css";
 import AboveHeader from "../../components/above_header/above_header";
@@ -6,30 +44,54 @@ import Footer from "../../components/footer/footer";
 import Title from "./Title/title";
 import Cards from "./cards/cards";
 import Banner from "./banner/banner";
-import Herosection from "./hero_section/herosection";
+import Hero_Section from "./hero_section/herosection";
 import Testimonial from "./testimonial/testimonial";
-import Facilities from "../../components/facilities/facilities"
- 
-function About () {
-    return(
-        <div>
-        <div><AboveHeader /></div>
-        <div><Header /></div>
-        <div><Herosection /></div>
-        <div><Banner /></div>
-        <div><Herosection /></div>
-        <div><Banner /></div>
-        <div className={styles.title}>
-           <Title />
-           </div>
-        <div className={styles.center}>
-            {/* <Testimonial /> */}
-          
-            <Cards />
-            <Facilities />
-        </div>
-        <div><Footer /></div>
-        </div>
-    )
+import Facilities from "../../components/facilities/facilities";
+import Gokul from "../about/Assets/gokul.webp";
+
+
+function About() {
+  return (
+    <div className={styles.aboutContainer}>
+      <AboveHeader />
+      <Header />
+
+      {/* Pass Props Dynamically */}
+      <Hero_Section
+        img = { Gokul }
+        title="About Gokul's"
+        desc="🍽️ Gokul's Sweets & Fast Food – Ambala Gokul's is a popular spot in Ambala known for its mouthwatering Indian sweets and a variety of fast food options."
+      />
+      
+      <Banner />
+
+      <Hero_Section
+       img = { Gokul } 
+        title="About Geeta Gopal Sanstha"
+        desc="Geeta Gopal Sanstha is a religious and charitable organization based in Ambala Cantt, Haryana. The organization is actively involved in various social welfare activities, including blood donation drives, assistance to the needy, daily satsangs (spiritual gatherings), and organizing pilgrimages. ​
+
+During the COVID-19 lockdown in 2020, Geeta Gopal Sanstha took the initiative to distribute cooked food to homeless individuals and laborers stranded due to the lockdown, demonstrating their commitment to community service. ​
+The Times of India
+
+The organization is located at Geeta Gopal Satsang Bhawan, Anaj Mandi, Ambala Cantt - 133001, Haryana, India. For more information, you can visit their official website: 
+"
+      />
+      
+      
+      <div className={styles.title}>
+        <Title />
+      </div>
+
+      <div className={styles.center}>
+        <Cards />
+        <Facilities />
+      </div>
+
+      {true && <Testimonial />} 
+
+      <Footer />
+    </div>
+  );
 }
+
 export default About;
--- client/src/pages/about/hero_section/herosection.jsx
+++ client/src/pages/about/hero_section/herosection.jsx
@@ -1,36 +1,62 @@
-import react from "react";
+// import react from "react";
+// import styles from "./herosection.module.css";
+// import Pic from "./assets/pic.png";
+
+// function Hero_Section() {
+//   return (
+//     <div className={styles.container}>
+//       <div className={styles.containertitle}>
+//        About Gokul's
+//       </div>
+//       <div className={styles.containercontent}>
+//         <div className={styles.containercontentleft}>
+//           <img src={Pic}></img>
+//         </div>
+//         <div className={styles.containercontentright}>
+//           <div className={styles.containercontentrighttext}>
+//             <p>
+//               🍽️ Gokul's Sweets & Fast Food – Ambala Gokul's is a popular spot
+//               in Ambala known for its mouthwatering Indian sweets and a variety
+//               of fast food options. From sponge rasgullas to crispy Manchurian
+//               and South Indian delicacies, it’s a go-to place for foodies. With
+//               outlets in Mahesh Nagar and near Congress Bhawan, it's loved for
+//               its taste, hygiene, and quick service.
+//             </p>
+//           </div>
+//           <div className={styles.containercontentrightbtn}>
+//             <button className={styles.orderbutton}>Order Now</button>
+//             <button className={styles.explorebutton}>Explore More</button>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+// export default Hero_Section;
+
+
+import React from "react";
 import styles from "./herosection.module.css";
-import Pic from "./assets/pic.png";
 
-function Hero_Section () {
-    return(
-        <div className={styles.container}>
-            <div className={styles.containertitle}>
-            Where Sweetness Meets Celebration
-            </div>
-            <div className={styles.containercontent}>
-                <div className={styles.containercontentleft}>
-                    <img src={Pic} ></img>
-                </div>
-                <div className={styles.containercontentright}>
-                    <div className={styles.containercontentrighttext}>
-                    <p>At Gokul’s, we believe that every moment of joy<br/>
-                    deserves to be cherished with something sweet and<br/>
-                    celebrated in a memorable way. Whether it's a family<br/>
-                    gathering, a grand wedding, or a simple indulgence,<br/>
-                    we bring you the finest handcrafted sweets and an<br/>
-                    elegant banquet hall to make your special occasions<br/>
-                    truly unforgettable.
-                    </p>
-                    </div>
-                    <div className={styles.containercontentrightbtn}>
-                        <button className={styles.orderbutton}>Order Now</button>
-                        <button className={styles.explorebutton}>Explore More</button>
-                    </div>
-                </div>
-            </div>
-
+function Hero_Section({ img, title, desc }) {
+  return (
+    <div className={styles.container}>
+      <div className={styles.containertitle}>
+        {title} {/* Dynamically pass title */}
+      </div>
+      <div className={styles.containercontent}>
+        <div className={styles.containercontentleft}>
+          <img src={img} alt={title} /> {/* Dynamically pass img */}
         </div>
-    )
+        <div className={styles.containercontentright}>
+          <div className={styles.containercontentrighttext}>
+            <p>{desc}</p> {/* Dynamically pass desc */}
+          </div>
+        
+        </div>
+      </div>
+    </div>
+  );
 }
+
 export default Hero_Section;
