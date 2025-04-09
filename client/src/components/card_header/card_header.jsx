d5a1b9a79c01242adaeb21bfeb01d0e924abe1a4
Abhishek <112743287+Abhishektarn@users.noreply.github.com>
2025-04-09 13:18:55 +0530

    Navigate on card_header.jsx

--- client/src/components/card_header/card_header.jsx
+++ client/src/components/card_header/card_header.jsx
@@ -23,14 +23,18 @@
 import React from "react";
 import styles from "./card_header.module.css";  
 import right_arro from "./assets/arrow.svg";
+import { useNavigate } from "react-router-dom";
 
 const CardHeader = ({ title = "Mero Gokul Sweets" }) => {
+  const navigate = useNavigate();
     return (
         <div className={styles.card_header}>
             <div className={styles.card_header_left}>{title}</div>
             <div className={styles.card_header_right}>
-                <div className={styles.card_header_right_text}>
+                <div className={styles.card_header_right_text} onClick={() => navigate("/")}>
+                    
                     VIEW ALL
+
                 </div>
                 <div className={styles.card_header_right_arrow}>
                     <img src={right_arro} alt="right_arro" />
