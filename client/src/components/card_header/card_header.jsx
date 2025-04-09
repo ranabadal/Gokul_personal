683cecbb7cf3e4077e6835d6e2ee5dea2abdd0af
Abhishek <112743287+Abhishektarn@users.noreply.github.com>
2025-04-09 17:31:00 +0530

    need to change in future the view all nav bar

--- client/src/components/card_header/card_header.jsx
+++ client/src/components/card_header/card_header.jsx
@@ -23,13 +23,15 @@
 import React from "react";
 import styles from "./card_header.module.css";  
 import right_arro from "./assets/arrow.svg";
+import { useNavigate } from "react-router-dom";
 
 const CardHeader = ({ title = "Mero Gokul Sweets" }) => {
+const navigate = useNavigate();
     return (
         <div className={styles.card_header}>
             <div className={styles.card_header_left}>{title}</div>
             <div className={styles.card_header_right}>
-                <div className={styles.card_header_right_text}>
+                <div className={styles.card_header_right_text} onClick={() => navigate("/banquets")}>   
                     VIEW ALL
                 </div>
                 <div className={styles.card_header_right_arrow}>
