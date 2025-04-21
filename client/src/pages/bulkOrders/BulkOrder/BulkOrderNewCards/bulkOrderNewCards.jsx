ac378b122910c1a5cdf5bc279455d43b4acb6580
Abhishek <112743287+Abhishektarn@users.noreply.github.com>
2025-04-21 14:31:02 +0530

    21

--- client/src/pages/bulkOrders/BulkOrder/BulkOrderNewCards/bulkOrderNewCards.jsx
+++ client/src/pages/bulkOrders/BulkOrder/BulkOrderNewCards/bulkOrderNewCards.jsx
@@ -4,15 +4,15 @@ import styles from "./bulkOrderNewCards.module.css";
 const BulkOrderNewCards = () => {
     
   return (
-    <div className={styles.container}>
-      <div className={styles.cards}>
-        <div className={styles.card1}>
-          <div className={styles.img}></div>
-          <div className={styles.cardConatent}>
-            <div className={styles.titleConatiner}>
-              <div className={styles.cardTitle}>Sweets</div>
-              <div className={styles.likeButton}></div>
-            </div>
+<div className={styles.container}>
+<div className={styles.cards}>
+<div className={styles.card1}>
+<div className={styles.img}></div>
+<div className={styles.cardConatent}>
+<div className={styles.titleConatiner}>
+<div className={styles.cardTitle}>Sweets</div>
+<div className={styles.likeButton}></div>
+</div>
             <div className={styles.ratingConatiner}></div>
             <div className={styles.desc}>
               Dummy text of the printing and typesetting industry. Lorem Ipsum
@@ -20,7 +20,7 @@ const BulkOrderNewCards = () => {
               when an unknown
             </div>
             <div className={styles.sizeButtons}>
-            
+
             </div>
             <div className={styles.priceConatiner}></div>
           </div>
