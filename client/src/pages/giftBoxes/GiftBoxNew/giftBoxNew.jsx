71df98dc17861ef1c3472f072cadd7788495e059
Abhishek <112743287+Abhishektarn@users.noreply.github.com>
2025-04-30 10:44:14 +0530

    new

---
+++ client/src/pages/giftBoxes/GiftBoxNew/giftBoxNew.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./giftBoxNew.module.css";
+
+const GiftBoxNew = () => {
+  return (
+    <div className={styles.giftBoxNew}>
+      <h1>Gift Box New</h1>
+      <p>This is the Gift Box New page.</p>
+    </div>
+  );
+}   
+
+export default GiftBoxNew;
