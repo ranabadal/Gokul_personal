dff5d411679a2cbeb4662c5b5afaab7726c7e8e7
Abhishek <112743287+Abhishektarn@users.noreply.github.com>
2025-05-20 10:38:33 +0530

    .

--- client/src/Const/Const.js
+++ client/src/Const/Const.js
@@ -1,3 +1,2 @@
-// export const BASE_URL = "http://localhost:8080";
-export const BASE_URL = "https://gokul.codroidhub.com/api";
-
+export const BASE_URL = "http://localhost:8080";
+// export const BASE_URL = "https://gokul.codroidhub.com/api";
