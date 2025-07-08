43328cc8f1206678630aac84b4b7790f46a1e96a
ranabadal <ranabadal3004@gmail.com>
2025-07-09 02:43:18 +0530

    fix: use node instead of nodemon in start script for Render

--- client/src/Const/Const.js
+++ client/src/Const/Const.js
@@ -1,3 +1,5 @@
-export const BASE_URL = "http://localhost:8080";
+// export const BASE_URL = "http://localhost:8080";
+export const BASE_URL = process.env.REACT_APP_BACKEND_URL;
+
 // export const BASE_URL = "https://gokul.codroidhub.com/api";
 // export const BASE_URL = "https://newgokulsweets.com/api";
